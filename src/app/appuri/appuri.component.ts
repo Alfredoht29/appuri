2b48ffddf69e039c956159b2f9939b6cfb5adfbf
Alfredoht <Emilioaperezvilla@outlook.com>
2022-08-02 10:55:18 -0500

    SeachAdded

--- src/app/appuri/appuri.component.ts
+++ src/app/appuri/appuri.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { tap, Observable, map } from 'rxjs';
-
+import { Observable,throwError} from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 @Component({
   selector: 'appuri',
   templateUrl: './appuri.component.html',
@@ -13,10 +13,12 @@ export class AppuriComponent implements OnInit {
   xd = { "id": 0 };
   xdany: any;
   searchj:any|undefined;
+  errormsg='';
+  condition=false;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-
+  
   }
 
   search(a: string) {
@@ -25,7 +27,12 @@ export class AppuriComponent implements OnInit {
     this.http.post(this.url, this.xd).toPromise().then(data => {
       console.log(data);
       this.searchj=data;
+      this.condition=false;
+     },  (error)=>{
+      if(error.status==404){
+        this.errormsg="El id buscado no existe";
+        this.condition=true;
+      }
     })
-
   }
 }
